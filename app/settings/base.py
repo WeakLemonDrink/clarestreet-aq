2d27c292c997e5993de95f9db3b2381747a887dd
Jonny Adcock <20461111+WeakLemonDrink@users.noreply.github.com>
2021-06-08 22:17:08 +0100

    fixed base_dir paths

--- app/settings/base.py
+++ app/settings/base.py
@@ -15,7 +15,8 @@ import os
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SETTINGS_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(SETTINGS_DIR))
 
 
 # Quick-start development settings - unsuitable for production
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "DIRS": [os.path.join(BASE_DIR, "app", "templates")],
         "APP_DIRS": True,
         'OPTIONS': {
             'context_processors': [
