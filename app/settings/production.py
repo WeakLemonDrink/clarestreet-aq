01f47bc1d11f7fbf3d705a652c75abb0ac7bb737
Jonny Adcock <20461111+WeakLemonDrink@users.noreply.github.com>
2021-06-08 10:37:59 +0100

    more settings

--- app/settings/production.py
+++ app/settings/production.py
@@ -14,8 +14,11 @@ ALLOWED_HOSTS = ['air-bs5.herokuapp.com']
 
 
 # Heroku db config
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+DATABASES = {
+    "default": {
+        dj_database_url.config(conn_max_age=500)
+    }
+}
 
 LOGGING = {
     'version': 1,
