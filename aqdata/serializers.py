02db175c44cc1852743cd2b7d013f8df62309259
Jonny Adcock <20461111+WeakLemonDrink@users.noreply.github.com>
2021-07-15 22:00:05 +0100

    added moving average fields to SensorDataSerializer

--- aqdata/serializers.py
+++ aqdata/serializers.py
@@ -9,9 +9,13 @@ class SensorDataSerializer(serializers.ModelSerializer):
     '''
 
     class Meta:
-        fields = ['esp8266id', 'software_version', 'upload_time',
-                  'BME280_humidity_pc', 'BME280_pressure_hpa',
-                  'BME280_temperature_deg_c', 'interval', 'max_micro', 'min_micro',
-                  'samples_per_sec', 'SDS_P1_ppm', 'SDS_P2_ppm', 'signal_dbm']
-        read_only_fields = ['upload_time']
+        fields = [
+            'esp8266id', 'software_version', 'upload_time', 'BME280_humidity_pc',
+            'BME280_pressure_hpa', 'BME280_temperature_deg_c', 'interval', 'max_micro',
+            'min_micro', 'samples_per_sec', 'SDS_P1_ppm', 'SDS_P2_ppm', 'signal_dbm',
+            'p1_ppm_24hr_moving_avg', 'p2_ppm_24hr_moving_avg'
+        ]
+        read_only_fields = [
+            'upload_time', 'p1_ppm_24hr_moving_avg', 'p2_ppm_24hr_moving_avg'
+        ]
         model = SensorData
